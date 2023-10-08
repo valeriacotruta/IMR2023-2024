ce7d693e460bb09b4e39fed1bf596b9f7843ff6f
valeriacotruta <cotrutavaleria31@gmail.com>
2023-10-08 18:53:26 +0300

    Tema 1

---
+++ Tema_1/Assets/Scripts/attack.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attack : MonoBehaviour
+{
+    private Animator mAnimator;
+    public float attackDistance = 0.25f;
+    public Transform target1;
+    public Transform target2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target1.position, target2.position);
+        if (distance < attackDistance)
+        {
+            mAnimator.SetTrigger("Attack");
+        }
+        else
+        {
+            mAnimator.SetTrigger("Idle");
+        }
+    }
+}
